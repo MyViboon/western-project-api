582929fe479dc49984440bd6f6d53a465e6f9509
viboon-project <viboon.project@gmail.com>
2025-02-26 14:58:26 +0700

    first commit

--- routes/category.js
+++ routes/category.js
@@ -1,16 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const { create, list, remove } = require('../controllers/category')
-const { authCheck, adminCheck } = require('../middlewares/authCheck')
+const express = require("express");
+const router = express.Router();
+const { create, list, remove } = require("../controllers/category");
+const { authCheck, adminCheck } = require("../middlewares/authCheck");
 
-// @ENDPOINT http://localhost:5001/api/category
-router.post('/category', authCheck, adminCheck, create)
-router.get('/category', list)
-router.delete('/category/:id', authCheck, adminCheck, remove)
+// @ENDPOINT https://western-project-api.vercel.app/api/category
+router.post("/category", authCheck, adminCheck, create);
+router.get("/category", list);
+router.delete("/category/:id", authCheck, adminCheck, remove);
 
-
-
-
-
-
-module.exports = router
+module.exports = router;
--- routes/product.js
+++ routes/product.js
@@ -1,30 +1,28 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 // Controllers
-const { create,
-    list,
-    read,
-    update,
-    remove,
-    listby,
-    searchFilters,
-    createImages,
-    removeImage
-} = require('../controllers/product')
-const { authCheck, adminCheck } = require('../middlewares/authCheck')
-// @ENDPOINT http://localhost:5001/api/product
-router.post('/product', create)
-router.get('/products/:count', list)
-router.get('/product/:id', read)
-router.put('/product/:id', update)
-router.delete('/product/:id', remove)
-router.post('/productby', listby)
-router.post('/search/filters', searchFilters)
+const {
+  create,
+  list,
+  read,
+  update,
+  remove,
+  listby,
+  searchFilters,
+  createImages,
+  removeImage,
+} = require("../controllers/product");
+const { authCheck, adminCheck } = require("../middlewares/authCheck");
+// @ENDPOINT https://western-project-api.vercel.app/api/product
+router.post("/product", create);
+router.get("/products/:count", list);
+router.get("/product/:id", read);
+router.put("/product/:id", update);
+router.delete("/product/:id", remove);
+router.post("/productby", listby);
+router.post("/search/filters", searchFilters);
 
-router.post('/images', authCheck, adminCheck, createImages)
-router.post('/removeimages', authCheck, adminCheck, removeImage)
+router.post("/images", authCheck, adminCheck, createImages);
+router.post("/removeimages", authCheck, adminCheck, removeImage);
 
-
-
-
-module.exports = router
+module.exports = router;
